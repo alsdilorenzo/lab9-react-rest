3ad2d5069e618832a7eba7a6669b186f4c62c2e5
alsdilorenzo <als.dilorenzo@gmail.com>
2020-05-29 11:33:27 +0200

    visual changes

--- client/src/components/TaskItem.js
+++ client/src/components/TaskItem.js
@@ -8,7 +8,7 @@ const TaskItem = (props) => {
     let {task, editTask, updateTask, deleteTask} = props;
 
     const toggleCheckbox = (ev, task) => {
-        if(ev.target.checked) {
+        if (ev.target.checked) {
             task.completed = true;
             updateTask(task);
         } else {
@@ -19,29 +19,31 @@ const TaskItem = (props) => {
 
     return (
         <ListGroup.Item id={task.id}>
-            <div className="d-flex w-100 justify-content-between">
-                <div className="custom-control custom-checkbox">
+            <div className="d-flex bd-highlight">
+                <div className="custom-control custom-checkbox p-2 flex-grow-1 bd-highlight">
                     <input type="checkbox"
                            className={task.important ? "custom-control-input important" : "custom-control-input"}
                            id={"check-t" + task.id} defaultChecked={task.completed}
                            onChange={(ev) => toggleCheckbox(ev, task)}/>
                     <label className="custom-control-label" htmlFor={"check-t" + task.id}>{task.description}</label>
                     {task.project && <span className="badge-pill badge-light ml-4">{task.project}</span>}
+                    {!task.privateTask && (
+                        <img src="https://image.flaticon.com/icons/svg/638/638887.svg"
+                             id="sharedIcon" width="18" height="18" alt=""/>)}
                 </div>
 
 
                 {task.deadline && (
                     <small
-                        className={task.deadline.isBefore(moment()) ? "date expired" : "date"}>{task.deadline.format("dddd, MMMM Do YYYY, h:mm a")} </small>
+                        className={task.deadline.isBefore(moment()) ? "date expired p-2 bd-highlight align-self-end" : "date p-2 bd-highlight align-self-end"}>{task.deadline.format("dddd, MMMM Do YYYY, h:mm a")} </small>
                 )}
 
-                {!task.privateTask && (
-                    <img src="https://image.flaticon.com/icons/svg/615/615075.svg" width="20" height="20" alt=""/>)}
-                <div>
-                    <Image width="20" height="20" className="img-button"
+
+                <div className="p-2 bd-highlight">
+                    <Image width="20" height="20" id="editIcon" className="img-button"
                            src="https://image.flaticon.com/icons/svg/1159/1159633.svg" alt=""
                            onClick={() => editTask(task)}/>
-                    <Image width="20" height="20" className="img-button"
+                    <Image width="20" height="20" id="deleteIcon" className="img-button"
                            src="https://image.flaticon.com/icons/svg/833/833262.svg" alt=""
                            onClick={() => deleteTask(task)}/>
                 </div>
